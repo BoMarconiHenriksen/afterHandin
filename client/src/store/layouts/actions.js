23e3baf8e477c1471a73ad0291b602291cca6101
BoMarconiHenriksen <bomarconi@gmail.com>
2019-06-06 13:04:43 +0200

    itemIndex is passed with the item when saving the headline

--- client/src/store/layouts/actions.js
+++ client/src/store/layouts/actions.js
@@ -60,6 +60,7 @@ export const removeItem = ({ commit }, payload) => {
 
 // Save header and text from input Field.
 export const saveInputField = ({ commit }, payload) => {
+  console.log(payload)
   commit('saveNameOfInputField', payload)
 }
 
